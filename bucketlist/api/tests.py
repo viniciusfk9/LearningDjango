df068b6ffdb8e9111f35b77d211d929c8f656571
Vinicius Ferreira de Souza <vinicius.fk9@gmail.com>
2017-06-27 01:28:09 -0300

    Add method to create a bucketlist

--- bucketlist/api/tests.py
+++ bucketlist/api/tests.py
@@ -1,3 +1,43 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from .models import Bucketlist
+
 
 # Create your tests here.
+
+
+class ModelTestCase(TestCase):
+    """This class defines the test suite for the Bucketlist model."""
+
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.bucketlist_name = "Write world class code"
+        self.bucketlist = Bucketlist(name=self.bucketlist_name)
+
+    def test_model_can_create_a_bucketlist(self):
+        """Test the Bucketlist model can create a bucketlist."""
+        old_count = Bucketlist.objects.count()
+        self.bucketlist.save()
+
+        new_count = Bucketlist.objects.count()
+        self.assertNotEqual(old_count, new_count)
+
+
+class ViewTestCase(TestCase):
+    """Test suite for the api views."""
+
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.client = APIClient()
+        self.bucketlist_data = {'name': 'Go to Ibiza'}
+        self.response = self.client.post(
+            reverse('create'),
+            self.bucketlist_data,
+            format="json")
+
+    def test_api_can_create_a_bucketlist(self):
+        """Test the api has bucket creation capability."""
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
--- bucketlist/api/views.py
+++ bucketlist/api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Bucketlist
+from .serializers import BucketlistSerializer
+
 
 # Create your views here.
+
+
+class CreateView(generics.ListCreateAPIView):
+    """This class defines the create behavior of our rest api."""
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
+
+    def perform_create(self, serializer):
+        """Save the post data when creating a new bucketlist."""
+        serializer.save()
+        
